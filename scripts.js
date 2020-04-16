068b897a23e7a82791db1fecd764fc2741aa674e
KovDan8757 <62889708+KovDan8757@users.noreply.github.com>
2020-04-16 18:31:12 +0200

    kész
    kész

--- scripts.js
+++ scripts.js
@@ -1,37 +1,38 @@
+
 let photo1 = {
     photo: "images/20171007_165604.jpg",    
     title: "Boti",
-    descraption: "Boti alszik",
+    description: "Boti alszik",
 };
 
 let photo2 = {
     photo: "images/20171020_153453.jpg",
     title: "Boti",
-    descraption: "Boti kalapban",
+    description: "Boti kalapban",
 };
 
 let photo3 = {
     photo: "images/20171027_154932.jpg",
     title: "Boti beöltözve",
-    descraption: "Boti útra készen",
+    description: "Boti útra készen",
 };
 
 let photo4 = {
     photo: "images/20171105_110851.jpg",
     title: "Boti kezeslábasban",
-    descraption: "Boti macinak öltözve",
+    description: "Boti macinak öltözve",
 };
 
 let photo5 = {
     photo: "images/20171110_104750.jpg",
     title: "Boti játszik",
-    descraption: "Boti a dobozban",
+    description: "Boti a dobozban",
 };
 
 let photo6 = {
     photo: "images/20180405_123612(0).jpg",
     title: "Boti a játszótéren",
-    descraption: "Boti hintázik",
+    description: "Boti hintázik",
 };
 
 
@@ -40,20 +41,49 @@ let currentPhoto = 0;
 
 let loadPhoto = (photoNumber) => {
 
-   $('#photo').attr('src', imagesData[currentphoto].photo);
+   $('#photo').attr('src', imagesData[currentPhoto].photo);
    $("#photo-title").text(imagesData[currentPhoto].title);
-   $("#photo-description").text(imagesData[currentPhoto].description);
+   $(".photo-description").text(imagesData[currentPhoto].description);
 }
 
 loadPhoto(currentPhoto);
-     $('#right-arrow').click(() => {
-         currentPhoto++;
-         loadPhoto(currentPhoto);
-     }) 
-
-     $('#left-arrow').click(() => {
+$("#right-arrow").click(() => {
+    if(currentPhoto < imagesData.length){
+        currentPhoto++;
+        loadPhoto(currentPhoto);
+    }
+});
+$("#left-arrow").click(() => {
+    if(currentPhoto >= 0){
         currentPhoto--;
         loadPhoto(currentPhoto);
-}) 
+    }
+});
 
-
+let counter = 0 ;
+imagesData.forEach(photo => {
+    $(".thumbnail").append(
+        '<div class="container" id="kontener-' + counter + '"><img src="' + photo.photo + '" data-number="' + counter +  '" id="containerPhoto"><blockquote class="speech-bubble">' + photo.title + '</blockquote></div>'
+        );
+        counter++;
+})
+$("#kontener-0").css("transform","scale(1.2)");
+$(".container").on( "click",function (event) {
+    currentPhoto = $(event.target).attr("data-number");
+    let id = "#kontener-" + currentPhoto;
+    let id3 = "#quote-" + currentPhoto;
+    
+    console.log(id);
+    let class2 = ".speech-bubble-" + currentPhoto;
+    loadPhoto(currentPhoto);
+    $(id).css("transform", "scale(1.2)");
+    $(id).css("border", "3px solid red");
+    $(class2).css("display","block");
+    for(let counter2 = 0; counter2 < imagesData.length; counter2++){
+        if(counter2 != currentPhoto){
+            let id2 = "#kontener-" + counter2;
+            $(id2).css("transform", "scale(1.0)");
+            $(id2).css("border", "none");
+        }
+    }
+});
